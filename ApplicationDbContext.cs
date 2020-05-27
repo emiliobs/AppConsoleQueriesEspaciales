bb2ff21df62654a5c8e759be3e2e937b3f69b019
Emilio <barrera_emilio@yahoo.es>
2020-05-27 13:52:14 +0100

    Migrations

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -4,35 +4,47 @@ using Microsoft.Extensions.Logging;
 using NetTopologySuite;
 using NetTopologySuite.Geometries;
 
-public class ApplicationDbContext : DbContext
-{
-  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-  {
-    optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=EFCoreEspacial;Integrated Security=true;",
-    x => x.UseNetTopologySuite())
-    .EnableSensitiveDataLogging(true)
-    .UseLoggerFactory(MyLoggerFactory);
-  }
+ public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
 
-  public static readonly ILoggerFactory MyLoggerFactory = LoggerFactory.Create(builder =>
- {
-   builder.AddFilter((category, level) => category == DbLoggerCategory.Database.Command.Name
-    && level == LogLevel.Information).AddConsole();
- });
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=EFCoreEspacial;Integrated Security=True",
+                    x => x.UseNetTopologySuite())
+                .EnableSensitiveDataLogging(true)
+                .UseLoggerFactory(MyLoggerFactory);
 
-  public DbSet<Restaurante> Restaurantes { get; set; }
+            base.OnConfiguring(optionsBuilder);
+        }
 
-  protected override void OnModelCreating(ModelBuilder modelBuilder)
-  {
-    var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
-    modelBuilder.Entity<Restaurante>().HasData(new List<Restaurante>()
-{
+        public static readonly ILoggerFactory MyLoggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+               .AddFilter((category, level) =>
+                   category == DbLoggerCategory.Database.Command.Name
+                   && level == LogLevel.Information)
+               .AddConsole();
+        });
 
-    new Restaurante(){Id = 1, Nombre = "Agor", Ciudad = "Santo Domingo", Ubicacion =  geometryFactory.CreatePoint(new  Coordinate(-69.9388777, 18.4839233))}
-    new Restaurante(){Id = 2, Nombre = "Sambil", Ciudad = "Santo Domingo", Ubicacion = geometryFactory.CreatePoint(new Coordinate(-69.9118804, 18.4826214))},
-    new Restaurante(){Id = 3, Nombre = "Adrian Tropical", Ciudad = "Santo Domingo", Ubicacion = geometryFactory.CreatePoint(new Coordinate(-69.9334673, 18.4718075))},
-    new Restaurante(){Id = 4, Nombre = "Restaurante El Cardenal", Ciudad = "Ciudad de México", Ubicacion = geometryFactory.CreatePoint(new Coordinate(-99.1353659,19.4336164))}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
 
-});
+            var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
 
-  }
+            modelBuilder.Entity<Restaurante>()
+                .HasData(
+                new List<Restaurante>()
+            {
+                new Restaurante(){Id = 1, Nombre = "Agora", Ciudad = "Santo Domingo", Ubicacion = geometryFactory.CreatePoint(new Coordinate(-69.9388777, 18.4839233))},
+                new Restaurante(){Id = 2, Nombre = "Sambil", Ciudad = "Santo Domingo", Ubicacion = geometryFactory.CreatePoint(new Coordinate(-69.9118804, 18.4826214))},
+                new Restaurante(){Id = 3, Nombre = "Adrian Tropical", Ciudad = "Santo Domingo", Ubicacion = geometryFactory.CreatePoint(new Coordinate(-69.9334673, 18.4718075))},
+                new Restaurante(){Id = 4, Nombre = "Restaurante El Cardenal", Ciudad = "Ciudad de México", Ubicacion = geometryFactory.CreatePoint(new Coordinate(-99.1353659,19.4336164))},
+                new Restaurante(){Id = 5, Nombre = "Tower Bridge", Ciudad = "London", Ubicacion = geometryFactory.CreatePoint(new Coordinate(-0.1453961, 51.5055424))}
+
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Restaurante> Restaurantes { get; set; }
+    }
---
+++ Migrations/20200527124458_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+
+namespace EFCoreQueriesEspaciales.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Restaurantes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Ciudad = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Ubicacion = table.Column<Point>(type: "geography", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurantes", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Restaurantes",
+                columns: new[] { "Id", "Ciudad", "Nombre", "Ubicacion" },
+                values: new object[,]
+                {
+                    { 1, "Santo Domingo", "Agora", (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (-69.9388777 18.4839233)") },
+                    { 2, "Santo Domingo", "Sambil", (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (-69.9118804 18.4826214)") },
+                    { 3, "Santo Domingo", "Adrian Tropical", (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (-69.9334673 18.4718075)") },
+                    { 4, "Ciudad de México", "Restaurante El Cardenal", (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (-99.1353659 19.4336164)") },
+                    { 5, "London", "Tower Bridge", (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (-0.1453961 51.5055424)") }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Restaurantes");
+        }
+    }
+}
